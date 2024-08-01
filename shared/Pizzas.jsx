b5281efc775f2bc4c4a27469ff77cccaf51d1769
Luc4s2303 <lenebia.lucas@gmail.com>
2024-08-01 16:45:11 -0300

    3

---
+++ shared/Pizzas.jsx
@@ -0,0 +1,62 @@
+import mussarela from "../assets/images/mussarela.png"
+import calabresa from "../assets/images/mussarela.png"
+import atum from "../assets/images/mussarela.png"
+import portuguesa from "../assets/images/mussarela.png"
+import frango from "../assets/images/mussarela.png"
+
+const Pizzas = [
+
+    [
+        {
+            "id": 1,
+            "descricao": "Pizza Margherita com molho de tomate, mozzarella, e manjericão fresco",
+            "imagem": mussarela
+        },
+        {
+            "id": 2,
+            "descricao": "Pizza Pepperoni com queijo extra e fatias de pepperoni picante",
+            "imagem": mussarela
+        },
+        {
+            "id": 3,
+            "descricao": "Pizza Quatro Queijos com mozzarella, gorgonzola, parmesão e provolone",
+            "imagem": mussarela
+        },
+        {
+            "id": 4,
+            "descricao": "Pizza Calabresa com cebola, azeitonas e orégano",
+            "imagem": mussarela
+        },
+        {
+            "id": 5,
+            "descricao": "Pizza Frango com Catupiry e milho verde",
+            "imagem": mussarela
+        },
+        {
+            "id": 6,
+            "descricao": "Pizza Portuguesa com presunto, ovos, cebola e pimentão",
+            "imagem": mussarela
+        },
+        {
+            "id": 7,
+            "descricao": "Pizza Vegetariana com cogumelos, pimentão, azeitonas e tomates",
+            "imagem": mussarela
+        },
+        {
+            "id": 8,
+            "descricao": "Pizza de Bacon com queijo cheddar e cebola caramelizada",
+            "imagem": mussarela
+        },
+        {
+            "id": 9,
+            "descricao": "Pizza de Atum com cebola, azeitonas e orégano",
+            "imagem": mussarela
+        },
+        {
+            "id": 10,
+            "descricao": "Pizza de Chocolate com morangos frescos",
+            "imagem":mussarela
+        }
+    ]
+    
+]

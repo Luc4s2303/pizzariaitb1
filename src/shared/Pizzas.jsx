5a43cd1d19a486a4d910235efbb71a11b21e6c6b
Luc4s2303 <lenebia.lucas@gmail.com>
2024-08-05 18:37:17 -0300

    1

--- src/shared/Pizzas.jsx
+++ src/shared/Pizzas.jsx
@@ -1,9 +1,12 @@
 import mussarela from "../assets/images/mussarela.png"
-import calabresa from "../assets/images/mussarela.png"
-import atum from "../assets/images/mussarela.png"
-import portuguesa from "../assets/images/mussarela.png"
-import frango from "../assets/images/mussarela.png"
-
+import calabresa from "../assets/images/calabresa.png"
+import queijos from "../assets/images/queijos.png"
+import atum from "../assets/images/atum.png"
+import frango from "../assets/images/frango.png"
+import portuguesa from "../assets/images/portuguesa.png"
+import bacon from "../assets/images/bacon.png"
+import cebola from "../assets/images/cebola.png"
+import chocolate from "../assets/images/chocolate.png"
 const Pizzas = [
 
     
@@ -15,27 +18,27 @@ const Pizzas = [
         {
             "id": 2,
             "descricao": "Pizza Pepperoni com queijo extra e fatias de pepperoni picante",
-            "imagem": mussarela
+            "imagem": calabresa
         },
         {
             "id": 3,
             "descricao": "Pizza Quatro Queijos com mozzarella, gorgonzola, parmesão e provolone",
-            "imagem": mussarela
+            "imagem": queijos
         },
         {
             "id": 4,
             "descricao": "Pizza Calabresa com cebola, azeitonas e orégano",
-            "imagem": mussarela
+            "imagem": atum
         },
         {
             "id": 5,
             "descricao": "Pizza Frango com Catupiry e milho verde",
-            "imagem": mussarela
+            "imagem": frango
         },
         {
             "id": 6,
             "descricao": "Pizza Portuguesa com presunto, ovos, cebola e pimentão",
-            "imagem": mussarela
+            "imagem": portuguesa
         },
         {
             "id": 7,
@@ -45,17 +48,17 @@ const Pizzas = [
         {
             "id": 8,
             "descricao": "Pizza de Bacon com queijo cheddar e cebola caramelizada",
-            "imagem": mussarela
+            "imagem": bacon
         },
         {
             "id": 9,
             "descricao": "Pizza de Atum com cebola, azeitonas e orégano",
-            "imagem": mussarela
+            "imagem": cebola
         },
         {
             "id": 10,
             "descricao": "Pizza de Chocolate com morangos frescos",
-            "imagem":mussarela
+            "imagem": chocolate
         },
     ]
     export default Pizzas;
